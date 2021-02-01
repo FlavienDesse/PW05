3a90198ae1a85b5e0de9dd70689ca69395b0ae84
MrFlavZz <flav621@gmail.com>
2021-02-01 13:37:15 +0100

    first push

--- src/main/java/fr/isen/java2/db/daos/FilmDao.java
+++ src/main/java/fr/isen/java2/db/daos/FilmDao.java
@@ -1,20 +1,52 @@
 package fr.isen.java2.db.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.isen.java2.db.entities.Film;
+import fr.isen.java2.db.entities.Genre;
 
 public class FilmDao {
 
-	public List<Film> listFilms() {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+    public List<Film> listFilms() {
 
-	public List<Film> listFilmsByGenre(String genreName) {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+        List<Film> listOfFilms = new ArrayList<>();
+        try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM film JOIN genre ON film.genre_id = genre.idgenre")) {
+                try (ResultSet results = statement.executeQuery()) {
+                    while (results.next()) {
+                        GenreDao genreDao = new GenreDao();
 
-	public Film addFilm(Film film) {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+                        Genre genre = genreDao.getById(results.getInt("genre_id"));
+                        Film film = new Film(
+                                results.getInt("idfilm"),
+                                results.getString("title"),
+                                results.getDate("release_date").toLocalDate(),
+                                genre,
+                                results.getInt("duration"),
+                                results.getString("director"),
+                                results.getString("summary"));
+                        listOfFilms.add(film);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listOfFilms;
+    }
+
+    public List<Film> listFilmsByGenre(String genreName) {
+        throw new RuntimeException("Method is not yet implemented");
+    }
+
+    public Film addFilm(Film film) {
+        throw new RuntimeException("Method is not yet implemented");
+    }
+
+
 }
--- src/main/java/fr/isen/java2/db/daos/GenreDao.java
+++ src/main/java/fr/isen/java2/db/daos/GenreDao.java
@@ -1,20 +1,85 @@
 package fr.isen.java2.db.daos;
 
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.isen.java2.db.entities.Genre;
 
 public class GenreDao {
 
-	public List<Genre> listGenres() {
-		throw new RuntimeException("Method is not yet implemented");
-	}
 
-	public Genre getGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+    public List<Genre> listGenres() {
+        List<Genre> listOfBooks = new ArrayList<>();
+        try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM genre")) {
+                try (ResultSet results = statement.executeQuery()) {
+                    while (results.next()) {
+                        Genre book = new Genre(results.getInt("idgenre"), results.getString("name"));
+                        listOfBooks.add(book);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listOfBooks;
+    }
 
-	public void addGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+
+    public Genre getGenre(String name) {
+    	
+    	Genre book = null;
+        try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM genre WHERE name = ?")) {
+                statement.setString(1, name);
+                try (ResultSet results = statement.executeQuery()) {
+                    if (results.next()) {
+                        return new Genre(results.getInt("idgenre"),results.getString("name"));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return book;
+    }
+
+    public void addGenre(String name) {
+
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			String sqlQuery = "insert into genre(name) "+"VALUES(?)";
+			try (PreparedStatement statement = connection.prepareStatement(
+					sqlQuery, Statement.RETURN_GENERATED_KEYS)) {
+				statement.setString(1, name);
+				statement.executeUpdate();
+
+			}
+		}catch (SQLException e) {
+			// Manage Exception
+			e.printStackTrace();
+		}
+
+    }
+
+    public Genre getById(Integer  genreId) {
+
+        try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from genre where idgenre=?")) {
+                statement.setInt(1, genreId);
+                try (ResultSet results = statement.executeQuery()) {
+                    if (results.next()) {
+                        return new Genre(results.getInt("idgenre"),results.getString("name"));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            // Manage Exception
+            e.printStackTrace();
+        }
+        return null;
+
+    }
 }
